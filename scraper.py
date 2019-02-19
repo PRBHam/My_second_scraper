12e80dc38ba1fc07a34961dc74f2b90e2f41f7ef
Pauline Roche <38589039+PRBHam@users.noreply.github.com>
2019-02-19 12:16:35 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,6 +19,8 @@ root.cssselect
 listofmatches=root.cssselect("a")
 for match in listofmatches:
   print(match)
+  print(lmxl.html.tostring(match))
+
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
