3e314f75d0053a52629610b5303280fc0713b7ed
Pauline Roche <38589039+PRBHam@users.noreply.github.com>
2019-02-19 12:02:56 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ print(html)
 root = lxml.html.fromstring(html)
 root.cssselect("a")
 #
-print(root.cssselect("div[align='left']"))
+print(root.cssselect("a"))
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
